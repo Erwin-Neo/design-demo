75c337a631f136e2d27383d13921fde84ed8d192
Erwin <edelweissvx@gmail.com>
2021-11-01 22:28:50 +0800

    add deep clone case

--- prototype-design/src/main/java/com/kyle/design/prototype/deep/ClientDrive.java
+++ prototype-design/src/main/java/com/kyle/design/prototype/deep/ClientDrive.java
@@ -1,7 +1,37 @@
-package com.kyle.design.prototype.deep;/**
+package com.kyle.design.prototype.deep;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
  * @author : Kyle
- * @email : edelweissvx@gmail.com
- * @description : 
  * @version : 1.0
- */public class Client {
+ * @email : edelweissvx@gmail.com
+ * @description :    深拷贝示例
+ */
+public class ClientDrive {
+
+    public static void main(String[] args) {
+        // 创建原型对象
+        ConcretePrototype prototype = new ConcretePrototype();
+        prototype.setProductID("NRT12910301923");
+        prototype.setProductName("Mac Pro");
+        List<String> tags = new ArrayList<String>();
+        tags.add("High Performance");
+        tags.add("Electronic Product");
+        prototype.setTags(tags);
+
+        //拷贝原型对象
+        ConcretePrototype cloneType = prototype.deepCloneTags();
+        cloneType.getTags().add("Delicate");
+
+        System.out.println("原型对象: " + prototype);
+        System.out.println("克隆对象: " + cloneType);
+        System.out.println("原型对象 vs 克隆对象: " + (prototype == cloneType));
+
+        System.out.println("原型对象的Tags:" + prototype.getTags());
+        System.out.println("克隆对象的Tags: " + cloneType.getTags());
+        System.out.println("原型对象Tags vs 克隆对象Tags: " + (prototype.getTags() == cloneType.getTags()));
+    }
 }
--- prototype-design/src/main/java/com/kyle/design/prototype/deep/ConcretePrototype.java
+++ prototype-design/src/main/java/com/kyle/design/prototype/deep/ConcretePrototype.java
@@ -1,7 +1,67 @@
-package com.kyle.design.prototype.deep;/**
+package com.kyle.design.prototype.deep;
+
+import lombok.Data;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
  * @author : Kyle
- * @email : edelweissvx@gmail.com
- * @description : 
  * @version : 1.0
- */public class ConcretePrototype {
+ * @email : edelweissvx@gmail.com
+ * @description :
+ */
+@Data
+public class ConcretePrototype implements Cloneable, Serializable {
+
+    private String productID;
+    private String productName;
+    private List tags;
+
+    @Override
+    public com.kyle.design.prototype.shallow.ConcretePrototype clone() {
+        try {
+            return (com.kyle.design.prototype.shallow.ConcretePrototype) super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public ConcretePrototype deepCloneTags() {
+        try {
+            ConcretePrototype result = (ConcretePrototype) super.clone();
+            result.tags = (List) ((ArrayList) result.tags).clone();
+            return result;
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public ConcretePrototype deepClone() {
+        try {
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            ObjectOutputStream oos = new ObjectOutputStream(bos);
+            oos.writeObject(this);
+
+            ByteArrayInputStream bis = new ByteArrayInputStream(bos.toByteArray());
+            ObjectInputStream ois = new ObjectInputStream(bis);
+
+            return (ConcretePrototype) ois.readObject();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "ConcretePrototype{" +
+                "productID='" + productID + '\'' +
+                ", productName='" + productName + '\'' +
+                ", tags=" + tags +
+                '}';
+    }
 }
