559bb331ad1959fbec0b7212736c3cddb05c2ff6
Erwin <edelweissvx@gmail.com>
2021-11-21 10:16:15 +0800

    update FlyweightDrive

--- flyweight-design/src/main/java/com/kyle/design/flyweight/general/FlyweightDrive.java
+++ flyweight-design/src/main/java/com/kyle/design/flyweight/general/FlyweightDrive.java
@@ -4,14 +4,14 @@ package com.kyle.design.flyweight.general;
  * @author : Kyle
  * @version : 1.0
  * @email : edelweissvx@gmail.com
- * @description :
+ * @description :  Client 测试类
  */
 public class FlyweightDrive {
 
     public static void main(String[] args) {
         Flyweight flyweightA = FlyweightFactory.getFlyweight("intrinsicStateA");
         Flyweight flyweightB = FlyweightFactory.getFlyweight("intrinsicStateB");
-        flyweightA.operation("extrinsicStateS");
+        flyweightA.operation("extrinsicStateA");
         flyweightB.operation("extrinsicStateB");
     }
 }
