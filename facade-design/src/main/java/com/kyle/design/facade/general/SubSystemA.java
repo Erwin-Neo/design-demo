bc7c7abdc22d178a5c7ac3b047c53b6bcf3b4685
Erwin <edelweissvx@gmail.com>
2021-12-14 23:38:50 +0800

    add facade general design case

---
+++ facade-design/src/main/java/com/kyle/design/facade/general/SubSystemA.java
@@ -0,0 +1,14 @@
+package com.kyle.design.facade.general;
+
+/**
+ * @author : Kyle
+ * @version : 1.0
+ * @email : edelweissvx@gmail.com
+ * @description :
+ */
+public class SubSystemA {
+
+    public void doA() {
+        System.out.println("do A stuff");
+    }
+}
---
+++ facade-design/src/main/java/com/kyle/design/facade/general/SubSystemB.java
@@ -0,0 +1,14 @@
+package com.kyle.design.facade.general;
+
+/**
+ * @author : Kyle
+ * @version : 1.0
+ * @email : edelweissvx@gmail.com
+ * @description :
+ */
+public class SubSystemB {
+
+    public void doB() {
+        System.out.println("do B stuff");
+    }
+}
